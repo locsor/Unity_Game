c752f8b594084959e8a6fad66607284e788324e6
Илья Павлов <locsor2@gmail.com>
2016-09-23 21:30:57 +0300

    First commit

---
+++ Assets/Move.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public Vector2 target;
+	public Camera cam;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x1 = 0, y1 = 0, x2, y2;
+		float y3 = 0.1f, x3 = 0.1f;
+		x2 = transform.position.x;
+		y2 = transform.position.y;
+		if (Input.GetMouseButtonDown(0))
+		{
+			target = cam.ScreenToWorldPoint(Input.mousePosition);
+			Debug.Log(target);
+		}
+		x1 = target.x;
+		y1 = target.y;
+		if (Mathf.Round (transform.position.x) < Mathf.Round (x1)) {
+			transform.position = new Vector2 (x2 + x3, y2);
+			Debug.Log('!');
+			x2 = transform.position.x;
+		}
+		if (Mathf.Round (transform.position.x) > Mathf.Round (x1)) {
+			transform.position = new Vector2 (x2 - x3, y2);
+			x2 = transform.position.x;
+		}
+		if (Mathf.Round (transform.position.y) < Mathf.Round (y1)) {
+			transform.position = new Vector2 (x2, y2 + y3);
+			Debug.Log('*');
+			y2 = transform.position.y;
+		}
+		if (Mathf.Round (transform.position.y) > Mathf.Round (y1)) {
+			transform.position = new Vector2 (x2, y2 - y3);
+			Debug.Log('*');
+			y2 = transform.position.y;
+		}
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+public class click_move : MonoBehaviour
+{
+    public Vector2 target;
+    public Camera cam;
+    bool check = false;
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x1 = 0, y1 = 0, x2, y2;
+        x2 = transform.position.x;
+        y2 = transform.position.y;
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = cam.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(target);
+            check = false;
+        }
+        x1 = target.x / 100;
+        y1 = target.y / 100;
+        if (!check)
+        {
+            transform.position = new Vector2(x2 + x1, y2 + y1);
+        }
+        if ((Mathf.Round(transform.position.x) == Mathf.Round(target.x)) && (Mathf.Round(transform.position.y) == Mathf.Round(target.y)))
+        {
+            x1 = 0;
+            y1 = 0;
+            check = true;
+        }
+    }
+}
